89ff6419f5630ca7df719bee8a335729394aa48e
zdodev <iver0945@gmail.com>
2021-12-09 21:42:21 +0900

    🎉: 프로젝트 시작

---
+++ ZdoGitHub/ZdoGitHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZdoGitHub
+//
+//  Created by Zero DotOne on 2021/12/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ZdoGitHub/ZdoGitHub/ZdoGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZdoGitHubApp.swift
+//  ZdoGitHub
+//
+//  Created by Zero DotOne on 2021/12/09.
+//
+
+import SwiftUI
+
+@main
+struct ZdoGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
